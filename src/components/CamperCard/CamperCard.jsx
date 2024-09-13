7b967c7f9c78dd7bfdde38850de5008366172573
Yuriy Yuriy <you.thunderstruck@gmail.com>
2024-09-13 22:59:43 +0300

    add css for catalog page

--- src/components/CamperCard/CamperCard.jsx
+++ src/components/CamperCard/CamperCard.jsx
@@ -7,70 +7,77 @@ export default function CamperCard({ item }) {
   return (
     <div className={styles.cardContainer}>
       <img className={styles.cardImg} src={item.gallery[0].original} alt="camper" />
-      <div>
+      <div className={styles.contentContainer}>
         <div className={styles.titleContainer}>
-          <p className={styles.name}>{item.name}</p>
+          <p className={styles.name}>{item.name.length>30?item.name.slice(0,31)+"...":item.name}</p>
           <p className={styles.price}>&euro; {item.price}.00</p>
+          <svg className={styles.heartIcon} width={24} height={24} >
+            <use href={`${sprite}#icon-heart_empty`} /> 
+          </svg>
         </div>
         <div className={styles.locationContainer}>
+          <svg className={styles.starIcon} width={16} height={16} >
+            <use href={`${sprite}#icon-strat_full`} /> 
+          </svg>
           <p>{item.rating} ({item.reviews.length} Reviews)</p>
-          <p>{item.location}</p>
+          <svg className={styles.mapIcon} width={16} height={16} >
+            <use href={`${sprite}#icon-map`} /> 
+          </svg>
+          <p className={styles.location}>{item.location}</p>
         </div>
         <p className={styles.description}>{item.description.slice(0, 53) + '...'}</p>
         <ul className={styles.tagList}>
           {item.transmission === 'automatic' && (
             <li className={styles.tagListItem}>
-              <svg className={styles.icon}>
+              <svg className={styles.icon} width={143} height={48} >
                 <use href={`${sprite}#icon-automatic_text`} /> {/* Corrected ID reference */}
               </svg>
             </li>
           )}
           {item.AC && (
             <li className={styles.tagListItem}>
-              <svg className={styles.icon}>
+              <svg className={styles.icon} width={87} height={48} >
                 <use href={`${sprite}#icon-ac_text`} /> {/* Corrected ID reference */}
               </svg>
             </li>
          )}
          {item.bathroom && (
             <li className={styles.tagListItem}>
-              <svg className={styles.icon}>
+              <svg className={styles.icon} width={148} height={48} >
                 <use href={`${sprite}#icon-bathroom_text`} /> {/* Corrected ID reference */}
               </svg>
             </li>
          )}
          {item.kitchen && (
             <li className={styles.tagListItem}>
-              <svg className={styles.icon}>
+              <svg className={styles.icon} width={123} height={48} >
                 <use href={`${sprite}#icon-kitchen_text`} /> {/* Corrected ID reference */}
               </svg>
             </li>
          )}
          {item.engine==="petrol" && (
             <li className={styles.tagListItem}>
-              <svg className={styles.icon}>
+              <svg className={styles.icon} width={109} height={48} >
                 <use href={`${sprite}#icon-petrol_text`} /> {/* Corrected ID reference */}
               </svg>
             </li>
          )}
          {item.radio && (
             <li className={styles.tagListItem}>
-              <svg className={styles.icon}>
+              <svg className={styles.icon} width={108} height={48} >
                 <use href={`${sprite}#icon-radio_text`} /> {/* Corrected ID reference */}
               </svg>
             </li>
          )} 
-         {item.bathroom && (
-            <li className={styles.tagListItem}>
-              <svg className={styles.icon}>
-                <use href={`${sprite}#icon-bathroom_text`} /> {/* Corrected ID reference */}
-              </svg>
-            </li>
-         )}         
+
         </ul>
-        <NavLink to={`/campers/${item.id}`}>
-          <ButtonMain className={styles.moreBtn}>Show More</ButtonMain>
-        </NavLink>
+
+    <div className={styles.linkWrapper}>
+      <NavLink to={`/campers/${item.id}`}>
+        <ButtonMain className={styles.moreBtn}>Show More</ButtonMain>
+      </NavLink>
+    </div>
+
       </div>
     </div>
   );
--- src/pages/CampersPage/CampersPaage.jsx
+++ src/pages/CampersPage/CampersPaage.jsx
@@ -1,36 +1,118 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCampers } from "../../redux/campersOps";
-import { selectedCampers, selectedError, selectedIsLoading } from "../../redux/selectors";
+import { selectedCampers, selectedIsLoading } from "../../redux/selectors";
 import { DNA } from "react-loader-spinner";
-import styles from './CampersPaage.module.css'
+import styles from './CampersPaage.module.css';
 import CamperCard from "../../components/CamperCard/CamperCard";
+import sprite from '../../assets/sprite.svg'; // Ensure this path is correct
 
 export default function CampersPage() {
-   const campers = useSelector(selectedCampers);
-   const isLoading = useSelector(selectedIsLoading)
-   const error = useSelector(selectedError)
-   console.log('campers :>> ', campers);
-   console.log('isLoading :>> ', isLoading);
-   console.log('error :>> ', error);
-   const dispatch = useDispatch()
-   useEffect(()=>{
-      dispatch(fetchCampers());
-      
-   }, [dispatch])
-   
-   return <div className={styles.campersContainer}>
-      <div className={styles.filterContainer}></div>
-      <div className={styles.listContainer}>
-         {isLoading&&<DNA/>}
-         {campers.total>0&&campers.items.map(item => {
-            return <li key={item.id} className={styles.camperCard}>
-               
-                  <CamperCard item={item}/>
+  const [selectedOptions, setSelectedOptions] = useState(new Set());
+  /*const [locationFilter, setLocationFilter] = useState(""); // For the input filter*/
+  const campers = useSelector(selectedCampers);
+  const isLoading = useSelector(selectedIsLoading);
+  console.log('campers :>> ', campers);
+  const dispatch = useDispatch();
 
-            </li>
-         })}
+  useEffect(() => {
+    dispatch(fetchCampers());
+  }, [dispatch]);
+
+  const handleOptionClick = (optionValue) => {
+    setSelectedOptions(prevState => {
+      const newSelectedOptions = new Set(prevState);
+      if (newSelectedOptions.has(optionValue)) {
+        newSelectedOptions.delete(optionValue);
+      } else {
+        newSelectedOptions.add(optionValue);
+      }
+      return newSelectedOptions;
+    });
+  };
+
+  const handleLocationChange = (event) => {
+    setLocationFilter(event.target.value);
+  };
+
+  const isSelected = (optionValue) => selectedOptions.has(optionValue);
+/*
+  // Function to filter campers based on selected options and location filter
+  const filteredCampers = campers?.items.filter(item => {
+    // Example filter logic: match selected options and location filter
+    const matchesOptions = selectedOptions.size === 0 || selectedOptions.has(item.someOption); // Replace 'someOption' with the actual property
+    const matchesLocation = locationFilter === "" || item.location.toLowerCase().includes(locationFilter.toLowerCase()); // Replace 'location' with the actual property
+
+    return matchesOptions && matchesLocation;
+  });*/
+
+  return (
+    <div className={styles.campersContainer}>
+      <div className={styles.filterContainer}>
+      <div className={styles.inputContainer}>
+         <svg className={styles.iconWrapper} width={20} height={20}>
+         <use href={`${sprite}#icon-map`} />
+         </svg>
+         <input
+         type="text"
+         className={styles.inputWithIcon}
+         placeholder="City"
+         onChange ={handleLocationChange}
+         />
       </div>
-    
-   </div>
+
+        <p className={styles.fieldName}>Filters</p>
+        <form className={styles.filterForm}>
+          <h3 className={styles.filterTitle}>Vehicle equipment</h3> 
+          {[ 
+            { value: "option1", icon: "icon-ac", label: "AC" },
+            { value: "option2", icon: "icon-diagram", label: "Automatic" },
+            { value: "option3", icon: "icon-cup", label: "Kitchen" },
+            { value: "option4", icon: "icon-tv", label: "TV" },
+            { value: "option5", icon: "icon-drop", label: "Bathroom" }
+          ].map(({ value, icon, label }) => (
+            <div
+              key={value}
+              className={`${styles.chekboxIcon} ${isSelected(value) ? styles.selected : ''}`}
+              onClick={() => handleOptionClick(value)}
+            >
+              <svg className={styles.icon} width={32} height={32}>
+                <use href={`${sprite}#${icon}`} />
+              </svg>
+              <p>{label}</p>
+            </div>
+          ))}
+        </form>
+
+        <form className={styles.filterForm}>
+          <h3 className={styles.filterTitle}>Vehicle type</h3>
+          {[ 
+            { value: "option6", icon: "icon-bi_grid-1x2", label: "Van" },
+            { value: "option7", icon: "icon-bi_grid-3x3-gap", label: "Fully Integrated" },
+            { value: "option8", icon: "icon-bi_grid", label: "Alcove" }
+          ].map(({ value, icon, label }) => (
+            <div
+              key={value}
+              className={`${styles.chekboxIcon} ${isSelected(value) ? styles.selected : ''}`}
+              onClick={() => handleOptionClick(value)}
+            >
+              <svg className={styles.icon} width={32} height={32}>
+                <use href={`${sprite}#${icon}`} />
+              </svg>
+              <p>{label}</p>
+            </div>
+          ))}
+        </form>
+      </div>
+
+      <div className={styles.listContainer}>
+        {isLoading && <DNA />}
+        {campers.items.length > 0 && campers.items.map(item => (
+          <li key={item.id} className={styles.camperCard}>
+            <CamperCard item={item} />
+          </li>
+        ))}
+      </div>
+    </div>
+  );
 }
